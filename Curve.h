6faad6b082933c06a0fb8ed5cd1b7088a1d2e1cc
Nguyễn Hoàng Bá Khiêm <48560151+Khiem472@users.noreply.github.com>
2019-03-30 13:59:48 +0700

    Sửa lại mã nguồn cho dễ đọc
    Gộp define và implement của header

--- Curve.h
+++ Curve.h
@@ -3,6 +3,7 @@
 #include "VertexDeque.h"
 #include <SFML/Graphics/Drawable.hpp>
 #include <SFML/Graphics/View.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
 #include <functional>
 
 class Curve : public sf::Drawable
@@ -21,13 +22,54 @@ public:
             lines[i].color = sf::Color::Black;
         }
     }
-    sf::Vector2f& operator[](int index);
-    const sf::Vector2f& operator[](int index) const;
-    void syncWithView(const sf::View& view);
-    size_t getPointsCount() const;
+
+    const sf::Vector2f& operator[](int index) const
+    {
+        return lines[index - indexOffset].position;
+    }
+
+    void syncWithView(const sf::View& view)
+    {
+        float left = view.getCenter().x - view.getSize().x / 2;
+        float right = view.getCenter().x + view.getSize().x / 2;
+        // Append more points
+        while (lines[lines.getVertexCount() - 1].position.x < right)
+        {
+            float x = lines[lines.getVertexCount() - 1].position.x + xStep;
+            float y = yGen(x);
+            lines.append(sf::Vertex{sf::Vector2f{x, y}, sf::Color::Black});
+            // doesn't affect indexOffset
+        }
+        // Prepend more points
+        while (lines[0].position.x > left)
+        {
+            float x = lines[0].position.x - xStep;
+            float y = yGen(x);
+            lines.prepend(sf::Vertex{sf::Vector2f{x, y}, sf::Color::Black});
+            indexOffset--;
+        }
+        // Trim right points
+        while (lines[lines.getVertexCount() - 1].position.x - xStep > right)
+        {
+            lines.removeLast();
+            // doesn't affect indexOffset
+        }
+        // Trim left points
+        while (lines[0].position.x + xStep < left)
+        {
+            lines.removeFirst();
+            indexOffset++;
+        }
+    }
+
+    size_t getPointsCount() const {
+        return lines.getVertexCount();
+    }
 
 private:
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
+        target.draw(lines, states);
+    }
 
 private:
     float xStep;
