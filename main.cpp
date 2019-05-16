314eebbd973d690588be9223ac7a3ef0587a6908
Nguyễn Hoàng Bá Khiêm <48560151+Khiem472@users.noreply.github.com>
2019-05-16 18:37:40 +0700

    CHẠY ĐI 0.8.3
    Mãi vẫn không xong : tiếp tục làm menu
    Ăn ở sạch sẽ : dọn dẹp mã nguồn

--- main.cpp
+++ main.cpp
@@ -7,10 +7,7 @@
 #include <stdlib.h>
 #include <iostream>
 
-float maxSpeed = 0.f, speedLimit = 200.f;
-
 int main() {
-    //render window
     sf::RenderWindow window(sf::VideoMode(800, 500), "CHAY DI!!!");
     window.setFramerateLimit(60);
 
@@ -31,17 +28,16 @@ int main() {
     mainM_s.setTexture(mainM_t);
 
     sf::Font font;
-    if (!font.loadFromFile("MAIAN.ttf")) {
-        std::cout << "Cannot open MAIAN.ttf\n";
+    if (!font.loadFromFile("Something Strange.ttf")) {
+        std::cout << "Cannot open font\n";
         return 1;
     }
 
-//    sf::Text mainM_text;
-//    mainM_text.setFont(font);
-//    mainM_text.setString(L"Bam phim bat ki...");
-//    mainM_text.setCharacterSize(20);
-//    mainM_text.setFillColor(sf::Color::Red);
-//    mainM_text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+    sf::Text scoreText("", font, 25);
+    scoreText.setPosition(282.f, 269.f);
+
+    sf::Text highScoreText("", font, 25);
+    highScoreText.setPosition(546.f, 269.f);
 
     sf::Texture instruction_t;
     instruction_t.loadFromFile("instruction.png");
@@ -54,15 +50,12 @@ int main() {
     sf::Sprite instruction1_s;
     instruction1_s.setTexture(instruction1_t);
     int insAlpha = 0;
-    //instruction1_s.setColor(sf::Color(255,255,255,insAlpha));
 
     sf::Texture youReady_t;
     youReady_t.loadFromFile("youReady.png");
     sf::Sprite youReady_s;
     youReady_s.setTexture(youReady_t);
     int readyAlpha = 0;
-
-    //instruction_s.setPosition(200.f, 500.f);
 
 
     //initialize properties for the curve
@@ -80,8 +73,6 @@ int main() {
                      yMagnitude3 * noiseGen.noise(xScaled * 0.8) +
                      yMagnitude4 * noiseGen.noise(xScaled * 1));
     };
-
-    //create the in-sight curve with the properties initialized
     Curve curve(1.f, window.getSize().x, yGen);
 
 
@@ -118,107 +109,69 @@ int main() {
     darkness_s.setTexture(darkness_t);
     darkness_s.setPosition(-1800.f, -500.f);
 
-//    sf::Texture flag_t;
-//    flag_t.loadFromFile("flag.png");
-//    sf::Sprite flag_s;
-//    flag_s.setTexture(flag_t);
-//    flag_s.setPosition(-60.f, -75.f);
-
     Obstacle obstacle1(obs1, position, positionNext),
              obstacle2(obs2, position, positionNext),
              obstacle3(obs3, position, positionNext);
 
     ////PHYSICS
     Physics physics;
-
-
-
     sf::View view = window.getView();
     view.setCenter(120, 0);
     curve.syncWithView(view);
-
-
-
-
-//    sf::Text seedText("Seed: " + std::to_string(seed), font, 1);
-//    seedText.setPosition(0, 0);
-//    seedText.setFillColor(sf::Color::Black);
-//    sf::Text distanceText;
-//    //distanceText.setPosition (0.f, 0.f);
-//    distanceText.setStyle    (sf::Text::Bold);
-//    distanceText.setFillColor(sf::Color::White);
-
-   // sf::Text curvePointsText{
-   //     "Curve points: " + std::to_string(curve.getPointsCount()), font, 16};
-   // curvePointsText.setPosition(0, 32);
-   // curvePointsText.setFillColor(sf::Color::Black);
-   // sf::Text positionText{
-   //     "Position: " + std::to_string(character.getPosition().x) + ", " +
-   //         std::to_string(character.getPosition().y),
-   //     font, 16};
-   // positionText.setPosition(0, 48);
-   // positionText.setFillColor(sf::Color::Black);
 
     sf::Clock clock;
     sf::Clock clock1, clock2, clock3, clockPlayed, clockBefore1, clockBefore2, clockBefore3;
     sf::Time  time1,  time2,  time3,  timePlayed, timeBefore1, timeBefore2, timeBefore3;
 
-    /*----------------------------------------------------------------------------------
-                                      MAIN LOOP
-    ----------------------------------------------------------------------------------*/
     bool gameOver = false, collided = false, gameStart = false,
          startRequest = false, gameInstructing = false, gameReady = false;
     int distanceRan = 0;
+    float maxSpeed = 0.f, speedLimit = 200.f;
+    int highScore = 0;
+
+    /*----------------------------------------------------------------------------------
+                                      MAIN LOOP
+    ----------------------------------------------------------------------------------*/
 
     while (window.isOpen()) {
         while (!gameOver) {
-            sf::Event event;
-            while (window.pollEvent(event)) {
-                //close the window
-                if (event.type == sf::Event::Closed)
+            sf::Event event1;
+            while (window.pollEvent(event1)) {
+                if (event1.type == sf::Event::Closed)
                     window.close();
-                else if (event.type == sf::Event::MouseButtonPressed || event.type == sf::Event::KeyPressed) {
-                    if (sf::Mouse::isButtonPressed(sf::Mouse::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+                else if (event1.type == sf::Event::MouseButtonPressed /*|| event1.type == sf::Event::KeyPressed*/) {
+                    if (sf::Mouse::isButtonPressed(sf::Mouse::Left) /*|| sf::Keyboard::isKeyPressed(sf::Keyboard::Space)*/) {
                         if (gameStart) {
                             character.up = !character.up;
                         }
                         sf::Vector2i localPosition = sf::Mouse::getPosition(window);
                         if (localPosition.x >= 359 && localPosition.x <= 440 && localPosition.y >= 431 && localPosition.y <= 470 || sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
-                            if (!gameInstructing) {
+                            if (!gameInstructing)
                                 gameInstructing = true;
-                            }
-                            else if (!gameReady) {
+
+                            else if (!gameReady)
                                 gameReady = true;
-                            }
-                            else if (!gameStart) {
+                            else if (!gameStart)
                                 startRequest = true;
-                            }
                         }
                     }
                 }
             }
 
 
-
-
-
-
             if (startRequest && character.getMoveSpeed() > 0.f && character.getMoveSpeed() < 6.f && character.getAngle() > 0 ) {
-                     gameStart = true;
-                     clock1.     restart();
-                     clock2.     restart();
-                     clock3.     restart();
-                     clockPlayed.restart();
+                gameStart = true;
+                clock1.     restart();
+                clock2.     restart();
+                clock3.     restart();
+                clockPlayed.restart();
             }
 
 
-            //std::cout << character.getMoveSpeed() << std::endl;
             ///////MOVE CHARACTER
             sf::Time elapsed = clock.restart();
             physics.a = Physics::G * sin(character.getAngle());
-            //if (physics.a >  210) physics.a =  210;
-            //if (physics.a < -210) physics.a = -210;
-            //std::cout << physics.a << std::endl;
+
             float instantVeclocity = physics.a * elapsed.asSeconds();
             if (speedLimit < 800.f)
                 speedLimit += timePlayed.asSeconds() * 2;
@@ -237,94 +190,33 @@ int main() {
 
             if (character.getMoveSpeed() > maxSpeed)
                 maxSpeed = character.getMoveSpeed();
-
-//            speedLimit = 200 + timePlayed.asSeconds() / 2;
-//            if (abs(character.getMoveSpeed()) > speedLimit) {
-//                if (character.up) {
-//                    character.setMoveSpeed(speedLimit);
-//                }
-//                else character.setMoveSpeed(-speedLimit);
-//            }
-
-            //std::cout << maxSpeed << std::endl;
-
-            //sf::Event start;
-            //while (window.pollEvent(start)) {
-                // if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-                //     gameStart = true;
-            //}
-
-            //std::cout << "t : " << elapsed.asSeconds() << "   alpha = " << character.getAngle() << "    a = " << physics.a << "    v = " << character.getMoveSpeed() << std::endl;
             if (!gameStart) {
                 view.setCenter(120,0);
                 instruction_s.setColor(sf::Color(255, 255, 255, alpha += i));
                 if (alpha >= 252) i = -3;
                 if (alpha <= 90)  i =  3;
-                //if (character.getMoveSpeed() > 200.f) character.setMoveSpeed(200.f);
-                //if (character.getMoveSpeed() < -200.f) character.setMoveSpeed(-200.f);
-                //curve.syncWithView(view);
             }
-    //        sf::Time elapsed = clock.restart();
+
             float moveAmount = 0;
-            //float y0 = curve[0].y;
-            // if (character.getMoveSpeed() > 200.f) {
-
-            //     float v0 = character.getMoveSpeed(),
-            //           t  = 0,
-            //           v0x,
-            //           v0y,
-            //           x = 0, y = 0;
-            //     v0x = v0 * cos(character.getAngle());
-            //     v0y = v0 * sin(character.getAngle());
-
-            //     //while (y < curve[0].y) {
-            //         t += 0.0165;
-            //         //x = curve[0].x + v0x*t;
-            //         //y = y0 + v0y*t - math::G*t*t/2.f;
-            //     //}
-            //     //man.setPosition(x, y);
-            //     std::cout << "JUMP!\n";
-            // }
-
             moveAmount += character.getMoveSpeed() * elapsed.asSeconds();
             if (!gameStart) distanceRan = 0;
             distanceRan += abs(moveAmount);
-            //distanceText.setString("m: ");
-            std::string unit = "m: ";
-//            if (distanceRan > 20000) unit = "km: ";
-            sf::Text distanceText{unit + std::to_string(distanceRan/40), font, 20};
-    //distanceText.setPosition (0.f, 0.f);
-            distanceText.setStyle    (sf::Text::Bold);
+            int score = distanceRan/40;
+            if (score > highScore) highScore = score;
+
+            std::string unit = "distance   ";
+            sf::Text distanceText{unit + std::to_string(score), font, 25};
+            distanceText.setPosition(645.f, 10.f);
             distanceText.setFillColor(sf::Color::White);
 
+            scoreText.setString(std::to_string(score) + "m");
+            highScoreText.setString(std::to_string(highScore) + "m");
+
             view.move(character.move(moveAmount));
-            //generate continuous curve as character moves
-//            viewX += moveAmount * cos(character.getAngle());
-            //viewY += moveAmount * sin(character.getAngle());
-            //viewX = clockPlayed.getElapsedTime().asSeconds() * 300;
-            //if (viewX < character.getPosition().x) viewX = character.getPosition().x - 40;
-            //viewY += clockPlayed.getElapsedTime().asSeconds() *
-            //view.setCenter(viewX, viewY);
             curve.syncWithView(view);
 
 
-
-
             if (gameStart) {
-                    //view.setCenter(character.getPosition().x-100, character.getPosition().y);
-            //curve.syncWithView(view);
-                // sf::Event event;
-                // while (window.pollEvent(event)) {
-                //     //close the window
-                //     if (event.type == sf::Event::Closed)
-                //         window.close();
-                //     //if player presses any button
-                //     else if (event.type == sf::Event::KeyPressed) {
-                //         //flip character
-                //         character.up = !character.up;
-                //     } // end if
-                // }
-
                 ////GENERATE OBSTACLES
                 timePlayed = clockPlayed.getElapsedTime();
                 time1 = clock1.getElapsedTime();
@@ -361,11 +253,10 @@ int main() {
                 }
 
                 float darknessMoved = elapsed.asSeconds() * maxSpeed * 4 / 5;
-                if (collided) {
-                    darknessMoved = 20.f;
+                if (collided && darknessMoved > 17.f) {
+                    darknessMoved = 17.f;
                 }
-                //if (darknessMoved > 10) darknessMoved = 10.f;
-                //if (darkness_s.getPosition().x > m
+
                 darkness_s.move(darknessMoved, 0.f);
                 if (darkness_s.getPosition().x >= view.getCenter().x - 500.f)
                     gameOver = true;
@@ -377,93 +268,61 @@ int main() {
 
 
             }
-    //            else if (event.type == sf::Event::KeyPressed)
-    //            {
-    //                if (event.key.code == sf::Keyboard::Up)
-    //                {
-    //                    character.addMoveSpeed(10.f);
-    //                   moveSpeedText.setString(
-    //                       "Move speed: " +
-    //                       std::to_string(character.getMoveSpeed()));
-    //                }
-    //                else if (event.key.code == sf::Keyboard::Down)
-    //                {
-    //                    character.addMoveSpeed(-10.f);
-    //                    moveSpeedText.setString(
-    //                        "Move speed: " +
-    //                        std::to_string(character.getMoveSpeed()));
-    //                }
-    //            }
 
-//            sf::Clock gmovrClock;
-//            sf::Time gmovrTime;
-
-
-            //if (collided) view.setCenter(character.getPosition().x-90, character.getPosition().y-90);
-            //std::cout << character.getMoveSpeed() << std::endl;
-            //if (collided) character.stop();
-
-            //if (gmovrTime.asSeconds() == 1.5) gameOver = true;
-
-
-            //window.setView(view);
             window.clear(sf::Color(11, 51, 24));
-            //background_s.move(-1.f, 0.f);
             window.draw(background_s);
 
             if (!gameReady) {
                 if (!gameInstructing)
                     window.draw(mainM_s);
                 else {
-                    //window.draw(gravityGradient_s);
-                    if (insAlpha < 250)
-                        insAlpha += 5;
+                    if (insAlpha < 250) insAlpha += 5;
                     instruction1_s.setColor(sf::Color(255, 255, 255, insAlpha));
                     window.draw(instruction1_s);
                 }
                 window.draw(instruction_s);
             }
             else if (!startRequest){
-                if (readyAlpha < 250)
-                    readyAlpha += 5;
+                if (readyAlpha < 250) readyAlpha += 5;
                 youReady_s.setColor(sf::Color(255, 255, 255, readyAlpha));
                 window.draw(youReady_s);
             }
-            //if (startRequest && character.getMoveSpeed() > 0.f && character.getMoveSpeed() < 6.f && character.getAngle() > 0 )
-            //{
-             //   character.stop();
-               // window.draw(grad_s);
-            //}
 
             window.setView    (view);
-            //window.draw       (background_s);
             window.draw       (curve);
             window.draw       (character);
 
             if (gameStart) {
-                //window.setView(view);
                 window.draw   (obstacle1);
                 window.draw   (obstacle2);
                 window.draw   (obstacle3);
                 window.draw   (darkness_s);
 
             }
-//            if (startRequest)
-//                window.draw(flag_s);
             window.setView(window.getDefaultView());
 
-            if (startRequest) {
-                window.draw   (distanceText);
-            }
+            if (startRequest) window.draw   (distanceText);
             window.display();
         }//while game is not over
 
+        sf::Event event2;
+        while (window.pollEvent(event2)) {
+            if (event2.type == sf::Event::Closed)
+                window.close();
+            else if (event2.type == sf::Event::MouseButtonPressed || event1.type == sf::Event::KeyPressed) {
+                if (sf::Mouse::isButtonPressed(sf::Mouse::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+                    sf::Vector2i localPosition = sf::Mouse::getPosition(window);
+                    std::cout << localPosition.x << "   " << localPosition.y << std::endl;
+                }
+            }
+        }
 
-        //window.clear(sf::Color(20,50,80,1));
         if (gmovrMAlpha < 250) gmovrMAlpha += 2;
         gmovrM_s.setColor(sf::Color(255, 255, 255, gmovrMAlpha));
-        window.draw            (gmovrM_s);
-        window.display         ();
+        window.draw(gmovrM_s);
+        window.draw(scoreText);
+        window.draw(highScoreText);
+        window.display();
 
 
 
